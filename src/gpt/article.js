6968dd7cd5595f53873fdf0d2c26ad24d8ac92f7
Dat Nguyen <datnguyen1215@gmail.com>
2023-07-22 00:14:43 -0700

    Regenerate article if failed for any reasons.

--- src/gpt/article.js
+++ src/gpt/article.js
@@ -7,25 +7,45 @@ import config from '@/config';
 const logger = createLogger('gpt.article');
 
 const generate = async title => {
-  const { article, env } = config();
+  try {
+    const { article, env } = config();
 
-  const openai = new OpenAIApi(new Configuration({ apiKey: env.OPENAI_KEY }));
+    const openai = new OpenAIApi(new Configuration({ apiKey: env.OPENAI_KEY }));
 
-  logger.info(`Generating outline for "${title}"...`);
-  const generatedOutline = await outline(title);
+    logger.info(`Generating outline for "${title}"...`);
+    const generatedOutline = await outline(title);
 
-  const sections = generatedOutline.outline.map(
-    (section, index) =>
-      `${index + 1}. ${section.text}\n` +
-      `${section.points
-        .map((x, i) => `${index + 1}.${i + 1}. ${x}`)
-        .join('\n')}`
-  );
+    const sections = generatedOutline.outline.map(
+      (section, index) =>
+        `${index + 1}. ${section.text}\n` +
+        `${section.points
+          .map((x, i) => `${index + 1}.${i + 1}. ${x}`)
+          .join('\n')}`
+    );
 
-  logger.info(`Generating article for "${title}"...`);
-  const promises = sections.map(
-    async section =>
-      await openai.createChatCompletion({
+    logger.info(`Generating article for "${title}"...`);
+    const promises = sections.map(
+      async section =>
+        await openai.createChatCompletion({
+          model: article.model,
+          messages: [
+            {
+              role: 'system',
+              content: prompts.personality
+            },
+            {
+              role: 'user',
+              content: prompts.section
+                .replaceAll('{title}', title)
+                .replaceAll('{outline}', sections.join('\n'))
+                .replaceAll('{section}', section)
+            }
+          ]
+        })
+    );
+
+    promises.push(
+      openai.createChatCompletion({
         model: article.model,
         messages: [
           {
@@ -34,39 +54,23 @@ const generate = async title => {
           },
           {
             role: 'user',
-            content: prompts.section
+            content: prompts.faq
               .replaceAll('{title}', title)
               .replaceAll('{outline}', sections.join('\n'))
-              .replaceAll('{section}', section)
           }
         ]
       })
-  );
+    );
 
-  promises.push(
-    openai.createChatCompletion({
-      model: article.model,
-      messages: [
-        {
-          role: 'system',
-          content: prompts.personality
-        },
-        {
-          role: 'user',
-          content: prompts.faq
-            .replaceAll('{title}', title)
-            .replaceAll('{outline}', sections.join('\n'))
-        }
-      ]
-    })
-  );
-
-  const results = await Promise.all(promises);
-  return {
-    title,
-    outline: sections.join('\n'),
-    content: results.map(x => x.data.choices[0].message.content).join('\n')
-  };
+    const results = await Promise.all(promises);
+    return {
+      title,
+      outline: sections.join('\n'),
+      content: results.map(x => x.data.choices[0].message.content).join('\n')
+    };
+  } catch (error) {
+    return generate(title);
+  }
 };
 
 export default generate;
