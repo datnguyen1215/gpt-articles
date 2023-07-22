a3d767d7f07f5f5ed9a50b66756d0e14091db7e4
Dat Nguyen <datnguyen1215@gmail.com>
2023-07-22 00:34:57 -0700

    Use gpt-4 model for generating outline.

--- multile-titles.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-npm start -- --titleFile 'titles.txt' --outlineModel 'gpt-3.5-turbo' --articleModel 'gpt-4'
---
+++ multiple-titles.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+npm start -- --titleFile 'titles.txt' --outlineModel 'gpt-4' --articleModel 'gpt-4'
