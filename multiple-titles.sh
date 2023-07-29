0d5fee3490847a9583a7ddb64c05c9caa28cef28
Dat Nguyen <datnguyen1215@gmail.com>
2023-07-29 04:58:19 -0700

    Add a commented line for running gpt-3.5-turbo.

--- multiple-titles.sh
+++ multiple-titles.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 npm start -- --titleFile 'titles.txt' --outlineModel 'gpt-4' --articleModel 'gpt-4'
+# npm start -- --titleFile 'titles.txt' --outlineModel 'gpt-3.5-turbo' --articleModel 'gpt-3.5-turbo'
